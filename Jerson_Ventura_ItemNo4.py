7637734065143d738d58021f3f0c815932ada8e4
JersonVentura <33196065+JersonVentura@users.noreply.github.com>
2017-10-30 01:10:25 +0800

    Add files via upload

---
+++ Jerson_Ventura_ItemNo4.py
@@ -0,0 +1,9 @@
+def sepia(oldPixel):
+    red = oldPixel.getred()
+    green = oldPixel.getGreen()
+    blue = oldPixel.getBlue()
+    newred = 0.393(100) + 0.769(150) + 0.189(200)
+    newgreen = 0.349(100) + 0.686(150) + 0.168(200)
+    newblue =  0.272(100) + 0.534(150) + 0.131(200)
+    newPixel = Pixel(newred, newblue, newgreen)
+    return newPixel
